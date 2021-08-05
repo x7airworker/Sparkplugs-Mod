9c4e4c7ba17147eac9713b9835af276ecd4e16a3
x7airworker <jake@jakes-co.de>
2021-08-05 19:20:53 +0200

    Fixed a bug which caused the Sparkplug to be working when the creation was lifted.

--- Scripts/Sparkplug.lua
+++ Scripts/Sparkplug.lua
@@ -3,20 +3,8 @@ Sparkplug.maxParentCount = 1
 Sparkplug.maxChildCount = 0
 Sparkplug.connectionInput = sm.interactable.connectionType.logic
 
-function dump(o)
-    if type(o) == 'table' then
-       local s = '{ '
-       for k,v in pairs(o) do
-          if type(k) ~= 'number' then k = '"'..k..'"' end
-          s = s .. '['..k..'] = ' .. dump(v) .. ','
-       end
-       return s .. '} '
-    else
-       return tostring(o)
-    end
- end
-
 function Sparkplug.server_onFixedUpdate(self, deltaTime)
+    if self.shape:getBody():isOnLift() then return end
     local inputs = self.interactable:getParents()
     if #inputs == 1 then
         local active = inputs[1]:isActive()
